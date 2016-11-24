441531be17bc2ca4a64fd2f2c1a149b1a40ed02d
flysky115 <434944800@qq.com>
2016-11-24 10:59:29 +0800

    修改

--- Spider/models.py
+++ Spider/models.py
@@ -1,5 +1,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+import uuid
 # Create your models here.
+import datetime
+class News(models.Model):
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4)
+    source=models.CharField(max_length=20)
+    author=models.CharField(max_length=10)
+    createTime=models.DateTimeField(auto_now_add=True,default=datetime.datetime.now())
+    updateTime=models.DateTimeField(auto_now=True,default=datetime.datetime.now())
+    class Meta:
+        app_label=""
+
--- app01/models.py
+++ app01/models.py
@@ -1,10 +1,11 @@
 from __future__ import unicode_literals
 
 import uuid
-
 from django.db import models
 
 # Create your models here.
+import datetime
+
 class Right(models.Model):
     id=models.UUIDField(default=uuid.uuid4,primary_key=True)
     url=models.URLField()
@@ -12,8 +13,8 @@ class Right(models.Model):
     editRight=models.IntegerField(default=1)
     updateRight=models.IntegerField(default=1)
     deleteRight=models.IntegerField(default=1)
-    createDate=models.DateField(auto_now_add=True)
-    updateDate=models.DateField(auto_now=True)
+    createDate=models.DateField(auto_now_add=True,default=datetime.datetime.now())
+    updateDate=models.DateField(auto_now=True,default=datetime.datetime.now())
 
 
 class user(models.Model):
@@ -23,15 +24,15 @@ class user(models.Model):
     pwd=models.CharField(max_length=50)
     username=models.CharField(max_length=20)
     usrRole=models.ForeignKey("userRole")
-    createDate=models.DateField(auto_now_add=True)
-    updateDate=models.DateField(auto_now=True)
+    createDate=models.DateField(auto_now_add=True,default=datetime.datetime.now())
+    updateDate=models.DateField(auto_now=True,default=datetime.datetime.now())
 
 class userRole(models.Model):
     id=models.UUIDField(default=uuid.uuid4,primary_key=True)
     role_desc=models.CharField(max_length=20)
     role_code=models.CharField(max_length=5)
     role_right=models.ManyToManyField("Right")
-    createDate=models.DateField(auto_now_add=True)
-    updateDate=models.DateField(auto_now=True)
+    createDate=models.DateTimeField(auto_now_add=True,default=datetime.datetime.now())
+    updateDate=models.DateField(auto_now=True,default=datetime.datetime.now())
 # class Meta:
 #     app_label=
