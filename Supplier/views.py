ed97ff1b274e85e13dbc857ca41d4ab6a9468696
flysky115 <434944800@qq.com>
2016-12-02 17:54:45 +0800

    修改

--- Supplier/views.py
+++ Supplier/views.py
@@ -18,18 +18,16 @@ def index(request):
        request.session["pageid"]=page_id
 
        currentPage=int(utils.GetData(request,"page")) if utils.GetData(request,"page") else 1
-       pageSize=3
+       pageSize=1
        start=(currentPage-1)*pageSize if currentPage>=1 else 0
        totalCount=supplier_DAC.getAllSupplierInfo().__len__()
        pagecount=divmod(totalCount,pageSize)[0] if divmod(totalCount,pageSize)[1]==0 else divmod(totalCount,pageSize)[0]+1
        end=currentPage*pageSize if currentPage<=pagecount else pagecount
-
        tempstr="""<div class="pagination">
                             <ul>
                             <li class="disabled">
                                     <a href="#">«</a>
                                 </li>"""
-
        for item in range(0,pagecount):
            tempstr+="""
                          <li class="active">
@@ -37,10 +35,7 @@ def index(request):
                          </li>
                  """.format(item+1,item+1)
        tempstr+=" <li class='disabled'><a href='#'>»</a> </li></ul></div>"
-
        stringLink=mark_safe(tempstr)
-
-
        return render_to_response(urlconfig.index,
                                  {
                                      'form':SupplierForm(),
@@ -66,102 +61,6 @@ def index(request):
 
 
 
-    # response = HttpResponse(content_type='application/vnd.ms-excel')
-    # response[
-    #     'Content-Disposition'] = 'attachment;filename={0}-{1}.xls'.format("", "")
-    # wb = xlwt.Workbook(encoding='utf-8')
-    # sheet_prd = wb.add_sheet('PRD')
-    # sheet_uat = wb.add_sheet('UAT')
-    # sheet_fat = wb.add_sheet('FAT')
-    #
-    # style_heading = xlwt.easyxf("""
-    #     font:
-    #         name Arial,
-    #         colour_index white,
-    #         bold on,
-    #         height 0xA0;
-    #     align:
-    #         wrap off,
-    #         vert center,
-    #         horiz center;
-    #     pattern:
-    #         pattern solid,
-    #         fore-colour 0x19;
-    #     borders:
-    #         left THIN,
-    #         right THIN,
-    #         top THIN,
-    #         bottom THIN;
-    #     """
-    #                             )
-    # style_body = xlwt.easyxf("""
-    #     font:
-    #         name Arial,
-    #         bold off,
-    #         height 0XA0;
-    #     align:
-    #         wrap on,
-    #         vert center,
-    #         horiz left;
-    #     borders:
-    #         left THIN,
-    #         right THIN,
-    #         top THIN,
-    #         bottom THIN;
-    #     """
-    #                          )
-    # style_green = xlwt.easyxf(" pattern: pattern solid,fore-colour 0x11;")
-    # style_red = xlwt.easyxf(" pattern: pattern solid,fore-colour 0x0A;")
-    # fmts = [
-    #     'M/D/YY',
-    #     'D-MMM-YY',
-    #     'D-MMM',
-    #     'MMM-YY',
-    #     'h:mm AM/PM',
-    #     'h:mm:ss AM/PM',
-    #     'h:mm',
-    #     'h:mm:ss',
-    #     'M/D/YY h:mm',
-    #     'mm:ss',
-    #     '[h]:mm:ss',
-    #     'mm:ss.0',
-    # ]
-    # style_body.num_format_str = fmts[0]
-    # # 1st line
-    # sheet_prd.write(0, 0, '供应商', style_heading)
-    # sheet_prd.write(0, 1, '销售人员', style_heading)
-    # sheet_prd.write(0, 2, '销售电话', style_heading)
-    # sheet_prd.write(0, 3, '技术人员', style_heading)
-    # sheet_prd.write(0, 4, '技术电话', style_heading)
-    # row = 1
-    # contents = supplier_DAC.getAllSupplierInfo()
-    # for content in contents:
-    #     sheet_prd.write(row, 0, content.name, style_body)
-    #     sheet_prd.write(row, 1, content.sales, style_body)
-    #     sheet_prd.write(row, 2, content.sales_phone, style_body)
-    #     sheet_prd.write(row, 3, content.engineer, style_body)
-    #     sheet_prd.write(row, 4, content.engineer_phone, style_body)
-    #     # if content.deploy_progress == u'已发布':
-    #     #     sheet_prd.write(row, 3, content.deploy_progress, style_green)
-    #     # else:
-    #     #     sheet_prd.write(row, 3, content.deploy_progress, style_red)
-    #     # sheet_prd.write(row, 4, content.jira_issue_no, style_body)
-    #     # sheet_prd.write(row, 5, str(content.create_user), style_body)
-    #
-    #     # 第一行加宽
-    #     sheet_prd.col(0).width = 100 * 50
-    #     sheet_prd.col(1).width = 200 * 50
-    #     sheet_prd.col(2).width = 50 * 50
-    #     sheet_prd.col(3).width = 50 * 50
-    #     sheet_prd.col(4).width = 200 * 50
-    #     sheet_prd.col(5).width = 50 * 50
-    #     sheet_prd.col(6).width = 200 * 50
-    #     row += 1
-    # output = StringIO.StringIO()
-    # wb.save(output)
-    # output.seek(0)
-    # response.write(output.getvalue())
-    # return response
 
 
 
