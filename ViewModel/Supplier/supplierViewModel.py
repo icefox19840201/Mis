884434ed4d568369b3b6025aedd174f298626c51
flysky115 <434944800@qq.com>
2016-12-16 13:34:06 +0800

    添加查看供应商详细,及viewModel

--- ViewModel/Supplier/supplierViewModel.py
+++ ViewModel/Supplier/supplierViewModel.py
@@ -29,3 +29,58 @@ class supplierViewMode:
     @SalesPhone.setter
     def salesPhone(self,phone):
         self.__salesPhone=phone
+    @property
+    def Enginner(self):
+        return self.__enginner
+    @Enginner.setter
+    def Enginner(self,value):
+        self.__enginner=value
+
+    @property
+    def EnginnerPhone(self):
+        return self.EnginnerPhone
+    @EnginnerPhone.setter
+    def EnginnerPhone(self,value):
+        self.__enginnerPhone=value
+
+    @property
+    def SupplierName(self):
+        return self.__supplierName
+    @SupplierName.setter
+    def SupplierName(self,value):
+        self.__supplierName=value
+
+    @property
+    def SysType(self):
+        return self.__sysType
+    @SysType.setter
+    def SysType(self,value):
+        self.__sysType=value
+
+    @property
+    def Address(self):
+        return self.__address
+    @Address.setter
+    def Address(self,value):
+        self.__address=value
+
+    @property
+    def SupplierManager(self):
+        return self.__supplierManager
+    @SupplierManager.setter
+    def SupplierManager(self,value):
+        self.__supplierManager=value
+
+    @property
+    def Supplier_phone(self):
+        return self.__Supplier_phone
+    @Supplier_phone.setter
+    def Supplier_phone(self,value):
+        self.__Supplier_phone=value
+
+    @property
+    def ZipCode(self):
+        return self.__zipCode
+    @ZipCode.setter
+    def ZipCode(self,value):
+        self.__zipCode=value
